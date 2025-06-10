e6efb25020b18b77b5da18d9dabb6d62fb406ef7
ryunosuke <ryunosekeinokuchi@gmail.com>
2025-06-10 15:41:33 +0900

    井口編集

--- gitparctice.java
+++ gitparctice.java
@@ -4,17 +4,19 @@ public class gitparctice {
         System.out.println("test2!!!!!!!!!!!");
 
         //adding
-        System.out.println("Hello Inokuchi");
+        System.out.println("イェーイ");
 
 
         // 今はtestブランチです
         // 新しい変更
 
+        
         // 次の変更
 
         // github上の変更
         // sekiguchi編集
         System.out.println("orehasawadaze");
         System.out.println("ekano_jinnsei");
+        System.out.println("あんぱん");
     }
 }
