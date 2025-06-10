5f1c7d7645b0d94465f544e4f3a3d271e23e30e0
ranu <131184347+RAnu-0512@users.noreply.github.com>
2025-06-10 15:22:29 +0900

    resolove

--- gitparctice.java
+++ gitparctice.java
@@ -1,15 +1,10 @@
 public class gitparctice {
     public static void main(String[] args) {
         System.out.println("Hello");
-<<<<<<< HEAD
 
-        // adding
-        System.out.println("Hello");
-=======
-        
         //adding
         System.out.println("Hello Inokuchi");
->>>>>>> 2c602af8f1a1f41d104841ade174359330c637d7
+
 
         // 今はtestブランチです
         // 新しい変更
